8bed8d7fa0d1c451f2440852625e132d50d6d0ae
Luis Perez <luisperez@MacBook-Pro-de-Luis.local>
2024-04-21 20:42:13 -0600

    finising the tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://tubular-cocada-6120bb.netlify.app/",
+  integrations: [preact()]
+});
